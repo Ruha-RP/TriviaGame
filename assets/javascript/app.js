d0e0b7b06fda8493f135c0d56de99d3ef33aa666
Ruha Peera <ruha.rizwan@gmail.com>
2017-12-14 05:46:22 -0500

    functional game with timer, counter for in/correct answers & question form visibility

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,26 @@
 //STEP 1: ***Getting the document ready***
 $(document).ready(function() {
 
-
-
+//Hiding the form at the beginning of the game
+$(".quizContent").hide();
 
 //STEP 2: ***Creation of a countdown timer***
 
 	//Declaring the number counter variable to 5
-	var number = 5;
+	var number = 10;
 
 	//Variable that hold the interval ID, when run function is executed
 	var intervalId;
 
 	//Defining the run function. It does two things: sets an interval of 1s and reduces the number by 1
 	function run() {
+		//show the form
+		$(".quizContent").show();
+		//set the interval
 		intervalId =setInterval(decrement, 1000);
+		//chnage html text to previous stats after restarting the game
+		$(".stats").html("Previous Stats: ");
+
 	};
 
 	//Defining the decrement function that was a parameter in the run function
@@ -22,7 +28,7 @@ $(document).ready(function() {
 	 	//number reduced by 1
 		number--;
 		//to display the decrementing values in html, as an h3 heading
-	 	$("#show-number").html("<h3>" + number + "</h3>");
+	 	$("#show-number").html("<h3>Time Remaining: " + number + "</h3>");
 
 	 	//when time's up...
 	 	if (number === 0) {
@@ -33,6 +39,8 @@ $(document).ready(function() {
 	 		//creating a new html element that shows the stats
 	 		$("#numCorrect").html("Correct answers: " + numCorrect);
 	 		$("#numIncorrect").html("Incorrect answers: " + numIncorrect);
+	 		//changing name of previous stats to current stats
+	 		$(".stats").html("Current Stats: ");
 	 	}
 	};
 
@@ -40,7 +48,9 @@ $(document).ready(function() {
 	function stop() {
 	      clearInterval(intervalId);
 	      //reset the number to 5 seconds if the player wants to restart the game
-	      number = 5;
+	      number = 10;
+	      //hiding the questions, to only display the stats
+	      $(".quizContent").hide();
 	};
 
 
@@ -54,7 +64,7 @@ $(document).ready(function() {
 
 //Creation of variables, objects or arrays
 
-//The questions object:
+//The questions object:( was attempting to see if this approached worked out better...)
 var questionBank = {
 	qOne: {
 		qOne:$("#qOne"),
@@ -70,9 +80,8 @@ var questionBank = {
 var numCorrect = 0;
 
 //the number of incorrect variable, in the beginning set as all incorrect
-var numIncorrect = 4;
+var numIncorrect = 5;
 
-//
 
 
 //4.***CREATION OF ON-CLICK EVENTS FOR EACH QUESTION***
@@ -127,6 +136,28 @@ $("#yesFour").on("click", function() {
 	//let's test it
 	console.log("The numIncorrect is" + numIncorrect);
 });
+
+
+//when the correct answer is clicked fro question 5...
+$("#yesFive").on("click", function() {
+	//the number of correct answer increment by 1
+	numCorrect++;
+	//let's test it
+	console.log("The numCorrect is" + numCorrect);
+	//when the user clicks the incorrect answer
+	numIncorrect--;
+	//let's test it
+	console.log("The numIncorrect is" + numIncorrect);
+});
+
+
+//4.***VISIBILITY OF FORM*** 
+//Visible: after clicking start button
+//This code is inserted at the beginning of the page and  in the .onclick method for the start button
+//Invisible: when time's up
+//This code is inserted in the stop function
+//Stats: Invisible during game play
+
 
 
 
